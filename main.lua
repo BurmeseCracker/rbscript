c72b7a43e86fdbb2936e8e700428efaa9e0fbb84
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2025-12-10 22:25:44 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
-local raw = {
-  ""
+local base = "https://raw.githubusercontent.com/BurmeseCracker/rbscript/refs/heads/main/"
+
+local scripts = {
+    "collectBill.lua",
+    "rushHour.lua",
+  
+    -- add more files here
 }
+
+for _, file in ipairs(scripts) do
+    local url = base .. file
+    local code = game:HttpGet(url)
+    loadstring(code)()
+end
+
+print("All scripts loaded!")
