ded229da8d5b10523fcdcd50d9691bc6824e0f1b
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2025-12-11 15:33:16 +0900

    Remove Tycoon ownership check from rushHour.lua
    Removed the check for player's Tycoon ownership and related kick logic.

--- rushHour.lua
+++ rushHour.lua
@@ -2,23 +2,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local workspace = game:GetService("Workspace")
 
--- Check your Tycoon
-local function GetMyTycoon()
-    local TycoonsFolder = workspace:WaitForChild("Tycoons")
-    for _, t in ipairs(TycoonsFolder:GetChildren()) do
-        if t:FindFirstChild("Owner") and t.Owner.Value == LocalPlayer then
-            return t
-        end
-    end
-    return nil
-end
 
-local MyTycoon = GetMyTycoon()
-
-if not MyTycoon then
-    -- Kick if no Tycoon belongs to you
-    LocalPlayer:Kick("You do not own a Tycoon, script disabled.")
-end
 
 -- Force Rush Hour module
 local RushHourModule = require(LocalPlayer.PlayerScripts.Source.Systems.RushHour)
