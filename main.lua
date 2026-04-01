7c5803f3ea837fabd32343bdb627e1d6df9fe761
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-04-01 17:55:37 +0900

    Refactor script loading by removing unnecessary loop
    Removed the while loop and print statement for script loading.

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
-while task.wait(2)
-do
-
 local base = "https://raw.githubusercontent.com/BurmeseCracker/rbscript/refs/heads/main/"
 
 local scripts = {
    
     "trackerv1.lua",
     "trackerv2.lua",
+   
     
 }
 
@@ -18,7 +16,4 @@ for _, file in ipairs(scripts) do
     local url = base .. file
     local code = game:HttpGet(url)
     loadstring(code)()
-end
-
-print("✅ All scripts loaded!")
 end
