f61b0716d9b9fc2d4f843ccbbbda1d3ab7ae10e3
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-04-08 12:03:03 +0900

    Update AutoCollectBattery.lua

--- AutoCollectBattery.lua
+++ AutoCollectBattery.lua
@@ -12,7 +12,7 @@ local PickUpRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Int
 local AdjustRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Tools"):WaitForChild("AdjustBackpack")
 
 -- Config
-local MAX_DIST = 100 -- အနီးနား 100 studs အတွင်း ရှာမည်
+local MAX_DIST = 20 -- အနီးနား 100 studs အတွင်း ရှာမည်
 local TARGET_NAMES = {
     ["Battery"] = true, 
     ["Battery Pack"] = true
