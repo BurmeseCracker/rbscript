df63534974342ad16a6defeeb7c00671790d4e33
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-04-12 22:11:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,5 @@
 local base = "https://raw.githubusercontent.com/BurmeseCracker/rbscript/refs/heads/main/"
-local RS = game:GetService("ReplicatedStorage")
 
--- [[ TARGETED DELETION ]] --
-local function remove(path)
-    if path then path:Destroy() end
-end
-
--- Delete Cutscenes
-remove(RS:FindFirstChild("Assets") and RS.Assets:FindFirstChild("Cutscenes"))
-
--- Delete Shake & ScreenEffects inside Modules > VFX
-local vfx = RS:FindFirstChild("Modules") and RS.Modules:FindFirstChild("VFX")
-if vfx then
-    remove(vfx:FindFirstChild("Shake"))
-    remove(vfx:FindFirstChild("ScreenEffects"))
-end
 
 -- [[ LOADER ]] --
 local scripts = {"modM.lua"}
